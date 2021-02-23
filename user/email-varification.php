6f4304c2b39926ad6815b8bdb354a67a9ff9183e
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-02-23 15:20:00 +0530

    Create email-varification.php

---
+++ user/email-varification.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- important meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0 ,user-scalable=no">
+
+
+    <!-- Title of the website-->
+    <title>Notes MarketPlace</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../images/favicon/favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="../css/fonts/fonts.css">
+
+
+    <!-- ================================================
+                        CSS Files 
+    ================================================= -->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/bootstrap/bootstrap.min.css">
+
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../css/user/email-varification.css">
+
+</head>
+
+<body>
+
+    <!-- Preloader -->
+    <div id="preloader">
+        <div id="status">&nbsp;</div>
+    </div>
+    <!-- Preloader Ends -->
+    <section>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        <img src="../images/logo/logo-dark.png" alt="Logo">
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>
+                        <h1> Email Verification </h1>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <h2>Dear Smith,</h2>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <h3>Thanks for Signing up! </h3>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <h3>Simply click below for email varification.</h3>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <button type="submit"><span class="text-center">Verify Email Address</span></button>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+    <!-- ================================================
+                        JS Files 
+    ================================================= -->
+
+    <!-- jQuery -->
+    <script src="../js/jquery.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="../js/bootstrap/bootstrap.min.js"></script>
+
+
+
+</body>
+
+</html>
