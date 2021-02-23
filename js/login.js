17056833f95e4eabaf3c59d312c63a3520ba05a9
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-02-23 10:20:31 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,16 +1,17 @@
 $(function() {
 
     var field = "";
-$("#show-hide").click(function() {
+    $("#show-hide").click(function() {
 
-   field = $("#inputPassword").attr("type");
-    if( field == "password"){
-        $("#inputPassword").attr("type","text");
-    }
-    else{
-        $("#inputPassword").attr("type","password");
-    }
+        field = $("#inputPassword").attr("type");
+            if( field == "password"){
+                $("#inputPassword").attr("type","text");
+            }
+            else{
+                $("#inputPassword").attr("type","password");
+            }
 
-});
+    });
+
 
 });
