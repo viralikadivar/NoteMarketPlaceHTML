91e5c7ad0194b83dda3400aad44995fbe7e2322b
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-03-03 16:16:54 +0530

    Create activation.php

---
+++ user/activation.php
@@ -0,0 +1,26 @@
+<?php
+
+require "../db_connection.php";
+global $connection;
+$token = $_REQUEST['token'];
+$id = sqrt($token);
+
+$query = "SELECT * FROM Users WHERE ID = $id  AND IsEmailVerified = 0 ";
+$result = mysqli_query($connection, $query);
+$verified = mysqli_num_rows($result);
+
+if ($verified)
+    {
+    $sql = "UPDATE Users SET IsEmailVerified = 1 WHERE ID = $id ";
+    $result = mysqli_query($connection, $sql);
+    if ($result)
+        {
+        header("location:../login.php");
+        }
+      else
+        {
+        header("location:../user/sign-up.php");
+        }
+    }
+
+?>
