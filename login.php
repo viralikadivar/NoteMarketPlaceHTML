55a05544a35a0249c8737197c4b035c4dbcf2779
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-03-03 16:16:59 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,63 @@
+<?php require "db_connection.php" ?>
+<?php
+
+$password_class = "";
+$warning_class = "text-hide";
+
+if (isset($_POST["submit"])) {
+
+    global $connection;
+
+    $email = $_POST["email"];
+    $password  = $_POST["password"];
+
+
+    $query = "SELECT * FROM Users WHERE EmailId = '$email'";
+    $query_result = mysqli_query($connection, $query);
+
+    // NO User with Given email id
+    if (!mysqli_num_rows($query_result)) {
+        $password_class = "wrong-password";
+        $warning_class = "";
+    }
+
+    // When get user with email id 
+    $user_detail = mysqli_fetch_assoc($query_result);
+    if (mysqli_num_rows($query_result)) {
+
+        // Password get matched 
+        if (password_verify($password, $user_detail["Password"])) {
+
+            $password_class = "";
+            $warning_class = "text-hide";
+
+
+            if ($user_detail['RoleID'] == 1 || $user_detail['RoleID'] == 2) {
+                header("Location:admin/admin-dashboard.php");
+                exit();
+            } else {
+                $userID = $user_detail['ID'];
+                $firstLoginQuery = "SELECT * FROM UserProfile WHERE UserID = $userID ";
+                $userHasUserId = mysqli_query($connection, $firstLoginQuery);
+                if (mysqli_num_rows($userHasUserId) == 1) {
+                    header("Location:user/search-notes.php");
+                    exit();
+                } else {
+                    header("Location:user/user-profile.php");
+                    exit();
+                }
+            }
+        }
+
+        // Password not match 
+        else {
+            $password_class = "wrong-password";
+            $warning_class = "";
+        }
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,55 +88,12 @@
 
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/login.css?version=1">
+    <link rel="stylesheet" href="css/login.css?version=3010">
 
 
 </head>
 
 <body>
-
-<?php require "db_connection.php" ?>
-<?php
-
-if(isset($_POST["submit"])){
-    
-    global $connection;
-
-    $email = $_POST["email"];
-    $password  = $_POST["password"];
-    $password_class = "";
-    $warning_class = "text-muted";
-
-    $query = "SELECT * FROM users WHERE EmailId = '$email' ";
-
-    $query_result = mysqli_query( $connection , $query );
-
-    if(!mysqli_num_rows( $query_result)){
-        $password_class = "wrong-password";
-        $warning_class = "";
-    }
-
-    $user_detail = mysqli_fetch_assoc($query_result);
-
-    if(mysqli_num_rows( $query_result)){
-
-        if($user_detail["Password"] !=  $password ){
-            $password_class = "wrong-password";
-            $warning_class = "";
-
-        }
-        else{
-        $password_class = "";
-        $warning_class = "text-muted";
-        header("Location:index.html"); 
-        exit();
-        }
-
-    }
-
-}
-
-?>
 
     <section id="login-page">
 
@@ -85,34 +102,33 @@ if(isset($_POST["submit"])){
 
             <!-- Logo -->
             <div class="logo text-center">
-            <img src="images/logo/logo-white.png" alt="Logo">
+                <img src="images/logo/logo-white.png" alt="Logo">
             </div>
             <!-- Form -->
             <div class="form-wrapper">
-                
+
                 <h1 class="text-center">Login</h1>
 
                 <h5 class="text-center">Enter your Email address and password to login</h5>
 
-                <form  action="login.php" method = "post">
+                <form action="login.php" method="post">
                     <div class="form-group">
                         <label for="inputEmail">Email</label>
-                        <input type="email" name="email" class="form-control" id="inputEmail" 
-                            placeholder="Enter your email" required>
-    
+                        <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Enter your email" required>
+
                     </div>
                     <div class="form-group <?php echo $password_class; ?>">
-                        <label for="inputPassword">Password<a href="forgot-password.html" id="forgot-password">Forgot Password?</a></label>
+                        <label for="inputPassword">Password<a href="forgot-password.php" id="forgot-password">Forgot Password?</a></label>
                         <input type="password" name="password" class="form-control" id="inputPassword" aria-describedby="passwordVarification" placeholder="Enter your password" required><img id="show-hide" src="images/form/eye.png" alt="eye">
-                        <small id="passwordVarification" class="<?php echo $warning_class; ?> form-text">The password that you've entered is incorrect</small>
+                        <small id="passwordVarification" class=<?php echo $warning_class; ?>>The password that you've entered is incorrect</small>
                     </div>
                     <div class="form-group form-check">
                         <input type="checkbox" class="form-check-input" id="check">
                         <label class="form-check-label" for="check">Remember Me</label>
                     </div>
                     <!-- <button type="submit" name="submit"><span class="text-center"><a href="user/user-dashboard.html">Login</a></span></button> -->
-                    <button type="submit" name="submit" ><span class="text-center">Login</span></button>
-                    <h4 class="text-center">Don't have an account? <a href="user/sign-up.html">Sign Up</a> </span></h4>
+                    <button type="submit" name="submit"><span class="text-center">Login</span></button>
+                    <h4 class="text-center">Don't have an account? <a href="user/sign-up.php">Sign Up</a> </span></h4>
                 </form>
             </div>
         </div>
