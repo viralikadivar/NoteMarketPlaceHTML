6eb60650f0f3310ef7399ebaceecf3815fab536c
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-03-07 16:36:12 +0530

    Create login.js

---
+++ mvc/js/login.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    var field = "";
+    $("#show-hide").click(function() {
+
+        field = $("#inputPassword").attr("type");
+            if( field == "password"){
+                $("#inputPassword").attr("type","text");
+            }
+            else{
+                $("#inputPassword").attr("type","password");
+            }
+
+    });
+
+
+});
