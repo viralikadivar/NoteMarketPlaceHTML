d167cfdeee074e62f823b2d903e3969ef5407a56
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-03-27 17:20:02 +0530

    Create db_connection.php

---
+++ mvc2/db_connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$server = "localhost";
+$username = "root" ;
+$password = "";
+$database = "NotesMarketPlace";
+
+$connection = mysqli_connect( $server , $username , $password , $database );
+
+if(!$connection) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+?>
