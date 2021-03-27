8dde0f9ecbb3a32e56eb37365cdc6811703ba632
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-03-27 17:05:43 +0530

    Update changePassword.js

--- mvc/js/user/changePassword.js
+++ mvc/js/user/changePassword.js
@@ -10,11 +10,84 @@ $(function() {
             $(this).parent().children("input").attr("type","password");
         }
 
-
-
-// At time of form validation do #successfull-login style.display=visible/none , initially make it "none"
-
-
     });
+
+    $(document).on('click' , '#submit' , function(event){
+
+        var password = $("#inputPassword").val();
+        var confirm_password = $("#confirmPassword").val();
+
+     
+        // Regular Expression for Password Validation 
+        var regExPasswordLength = /([a-z A-Z 0-9 !@#$%^&*_~]){6,24}/;
+        var regExPasswordLowercase = /[a-z]+/;
+        var regExPasswordUpperCase = /[A-Z]+/;
+        var regExPasswordDigit = /[0-9]+/;
+        var regExPasswordSecialCharacters = /[!@#$%^&*_~]+/;
+        var isPassVerified = false ;
+        var validationMessage = "";
+
+        // Password Validation
+        if(regExPasswordLength.test(password)) {
+          if(password.length <= 26) {
+            if(!password.includes(" ")) {
+            if(regExPasswordLowercase.test(password)){
+                if(regExPasswordUpperCase.test(password)){
+                    if(regExPasswordDigit.test(password)){
+                        if(regExPasswordSecialCharacters.test(password)){
+                            isPassVerified = true ;
+                            validationMessage = "" ;
+                        }else {
+                            isPassVerified = false ;
+                            validationMessage = "Atleast contain one Special characters e.g. @#$%";
+                        }
+                    }else {
+                        isPassVerified = false ;
+                        validationMessage = "Atleast contain one Digit(0-9) characters";
+                    }
+                }else {
+                    isPassVerified = false ;
+                    validationMessage = "Atleast contain one Uppercase(A-Z) characters";
+                }
+            }else {
+                isPassVerified = false ;
+                validationMessage = "Atleast contain one lowercase(a-z) characters";
+            }
+        } else {
+            isPassVerified = false ;
+            validationMessage = "Password must not contain blank space ";
+        }
+          } else {
+            isPassVerified = false ;
+            validationMessage = "Password is too long ";
+          }
+        } else {
+            isPassVerified = false ;
+            validationMessage = "Atleast contain 6 characters";
+        }
+
+        if(!isPassVerified){
+            $("#inputPassword").parent().addClass("wrong-info");
+            $("#inputPassword").parent().removeClass("validation");
+            $("#passwordValidation").html(validationMessage);
+            event.preventDefault();
+            exit();
+        } else{
+            $("#inputPassword").parent().removeClass("wrong-info");
+            $("#inputPassword").parent().addClass("validation");
+            $("#passwordValidation").html(validationMessage);
+        }
+
+        // Password Matching 
+        if(password != confirm_password )  {
+            $("#confirmPassword").parent().addClass("wrong-info");
+            $("#confirmPassword").parent().removeClass("validation");
+            event.preventDefault();
+        }else {
+            $("#confirmPassword").parent().removeClass("wrong-info");
+            $("#confirmPassword").parent().addClass("validation");
+        }
+
+    }); 
 
 });
