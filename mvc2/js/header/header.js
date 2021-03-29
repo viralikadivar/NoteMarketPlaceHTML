7f1990b025db6f61e67773a2785faf512383640b
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-03-29 21:06:14 +0530

    Update header.js

--- mvc2/js/header/header.js
+++ mvc2/js/header/header.js
@@ -3,7 +3,7 @@
 ============================================ */
 $(window).on('load', function () { // makes sure that whole site is loaded
     $('#status').fadeOut();
-    $('#preloader').delay(400).fadeOut('slow');
+    $('#preloader').fadeOut('fast');
 });
 
 $(function() {
