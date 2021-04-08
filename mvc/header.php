d304cc48ccda37c45d6b7718242b2da04964eca7
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-04-08 17:00:22 +0530

    Update header.php

--- mvc/header.php
+++ mvc/header.php
@@ -52,7 +52,7 @@ if(isset($_SESSION['userRoleID']) && !empty($_SESSION['userRoleID'])) {
         echo '<header id="header">
         <nav class="navbar white-navbar navbar-expand-lg">
         <div class="container navbar-wrapper">
-            <a class="navbar-brand" href="../index.php">
+            <a class="navbar-brand" href="'.$fileURL.'index.php">
                 <img class="img-responsive" src="http://localhost/NotesMarketPlace/NoteMarketPlaceHTML/mvc/images/logo/logo-dark.png" alt="logo">
             </a>
 
