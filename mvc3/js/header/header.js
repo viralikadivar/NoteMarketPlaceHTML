105daaf6c0a5915fef8431a47b6b11ad54501227
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-04-09 15:42:26 +0530

    Create header.js

---
+++ mvc3/js/header/header.js
@@ -0,0 +1,24 @@
+/* ===========================================
+                Preloader
+============================================ */
+$(window).on('load', function () { // makes sure that whole site is loaded
+    $('#status').fadeOut();
+    $('#preloader').fadeOut('fast');
+});
+
+$(function() {
+    $("#header > nav.mobile-navbar > button").click("slow" ,function(){
+        $("nav").toggleClass("nav-medium");
+        $(this).toggleClass("open-close");
+    });
+});
+
+$(function(){
+    var count=0;
+    $("#user-menu").click("2000",function() {
+        if(count != 0){
+        $(this).parents("li").toggleClass("drop-down-img");
+        }
+        count++;
+    });
+});
