7425af81ed54a9f39c3135ea2cd15ffaaead4e29
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-04-10 16:45:12 +0530

    Update admin-profile.js

--- mvc3/js/admin/admin-profile.js
+++ mvc3/js/admin/admin-profile.js
@@ -9,17 +9,69 @@ $(function () {
     let value = $(this).attr("value");
     $("input[name='phoneCode']").val(value);
 
-    value = '+'+value + '<img src="../images/form/arrow-down.png" alt="Down">';
+    value =
+      "+" + value + '<img src="../images/form/arrow-down.png" alt="Down">';
     $("#phone-code").html(value);
   });
-});
 
-$(function () {
-    $(".phoneCodeAdmin li").click(function () {
-      let value = $(this).attr("value");
-      $("input[name='phoneCode']").val(value);
-  
-      value = '+'+ value + '<img src="../../images/form/arrow-down.png" alt="Down">';
-      $("#phone-code").html(value);
-    });
+  $(".phoneCodeAdmin li").click(function () {
+    let value = $(this).attr("value");
+    $("input[name='phoneCode']").val(value);
+
+    value =
+      "+" + value + '<img src="../../images/form/arrow-down.png" alt="Down">';
+    $("#phone-code").html(value);
   });
+
+  $('button[name="submit"]').click(function (event) {
+    let firstName = $("#first-name").val();
+    let lastName = $("#last-name").val();
+    let emailID = $("#email").val();
+    let phoneCode = $("#phoneCode").val();
+    let phoneNumber = $("#Phone-number").val();
+
+    // FirstName Validation
+    if (/^[0-9]+$/.test(firstName) || firstName == "") {
+      $("#first-name").css("border-color", "red");
+      event.preventDefault();
+    } else {
+      $("#first-name").css("border-color", "#d1d1d1");
+    }
+
+    // LastName Validation
+    if (/^[0-9]+$/.test(lastName) || lastName == "") {
+      $("#last-name").css("border-color", "red");
+      event.preventDefault();
+    } else {
+      $("#last-name").css("border-color", "#d1d1d1");
+    }
+
+    // Regular  Expression of an Email
+    var regExEmail = /^([a-z0-9\.-_~]+)@([a-z0-9-]+).([a-z]{2,20})(.[a-z]{2,20})?$/;
+
+    // EmailId Validation
+    if (!regExEmail.test(emailID) || emailID == "") {
+      $("#email").css("border-color", "red");
+      event.preventDefault();
+    } else {
+      $("#email").css("border-color", "#d1d1d1");
+    }
+
+    // Phone code Validation
+    if (phoneCode == "") {
+      $("#phone-code").css("border-color", "red");
+      event.preventDefault();
+    } else {
+      $("#phone-code").css("border-color", "#d1d1d1");
+    }
+
+    // Phone Number Validation 
+    if (!/^[0-9]{10}$/.test(phoneNumber) || phoneNumber== "") {
+      $("#Phone-number").css("border-color", "red");
+      event.preventDefault();
+    } else {
+      $("#Phone-number").css("border-color", "#d1d1d1");
+    }
+
+  });
+});
