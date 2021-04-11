2712ddaa546f7dce000ce088c5106aaddec6c305
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-04-11 15:54:18 +0530

    Create manage-fileds.js

---
+++ mvc3/js/admin/manage-fileds.js
@@ -0,0 +1,33 @@
+$(function() {
+
+    $('button[name="submit"]').click(function(event) {
+
+       
+        let name =  $("#title").val();
+        let description = $("#description").val();
+        let countryCode = $("#country-code").val();
+
+        if(name == "" || /^[0-9]+$/.test(name)){
+            $("#title").css("border-color","red");
+            event.preventDefault();
+        } else{
+            $("#title").css("border-color","#d1d1d1");
+        }
+
+        if(description == ""){
+            $("#description").css("border-color","red");
+            event.preventDefault();
+        } else{
+            $("#description").css("border-color","#d1d1d1");
+        }
+
+        if(countryCode == ""){
+            $("#country-code").css("border-color","red");
+            event.preventDefault();
+        } else{
+            $("#country-code").css("border-color","#d1d1d1");
+        }
+
+    });
+
+});
