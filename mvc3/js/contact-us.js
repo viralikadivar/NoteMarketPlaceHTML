be259b95b58593afe0af3c8cd7059b45f49b463f
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-04-12 17:37:54 +0530

    Create contact-us.js

---
+++ mvc3/js/contact-us.js
@@ -0,0 +1,47 @@
+$(function () {
+
+  $("button[name='submit']").click(function (event) {
+
+    var fullName = $("#fullName").val();
+    var emailID = $("#inputEmail").val();
+    var subject = $("#subject").val();
+    var comments = $("#comments-questions").val();
+
+    // First Name Validation 
+    if(/^[0-9]+$/.test(fullName)){
+        $("#fullName").css("border-color","red");
+        event.preventDefault();
+    } else{
+        $("#fullName").css("border-color","#d1d1d1");
+    }
+
+     // Regular  Expression of an Email
+     var regExEmail = /^([a-z0-9\.-_~]+)@([a-z0-9-]+).([a-z]{2,20})(.[a-z]{2,20})?$/;
+
+     // EmailId Validation 
+     if(!regExEmail.test(emailID)){
+        $("#inputEmail").css("border-color","red");
+        event.preventDefault();
+    } else {
+        $("#inputEmail").css("border-color","#d1d1d1");
+    }
+
+    // Subject validation 
+    if(subject == ""){
+        $("#subject").css("border-color","red");
+        event.preventDefault();
+    } else {
+        $("#subject").css("border-color","#d1d1d1");
+    }
+
+    // Comments Validation 
+    if(comments == "") {
+        $("#comments-questions").css("border-color","red");
+        event.preventDefault();
+    } else {
+        $("#comments-questions").css("border-color","#d1d1d1");
+    }
+    
+  });
+
+});
