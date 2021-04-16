e1f8b7ea54649a477632271fc48f75820f0b0642
viralikadivar <77288613+viralikadivar@users.noreply.github.com>
2021-04-16 12:59:58 +0530

    Update search-notes.php
    modifying pagination

--- mvc3/search-notes.php
+++ mvc3/search-notes.php
@@ -37,7 +37,7 @@ session_start();
     <link rel="stylesheet" href="css/header-footer/footer.css">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/search-notes.css?version=231250">
+    <link rel="stylesheet" href="css/search-notes.css?version=2312225250">
 
 </head>
 
@@ -223,193 +223,9 @@ session_start();
         </section>
         <!-- Search Fields Ends -->
 
-        <!-- Search Result -->
-        <section id="search-result">
-
-            <?php
-            $noOFResultPerPage = 9;
-            ?>
-            <div class="container">
-
-                <?php
-
-                if (!isset($_GET['page'])) {
-                    $currentPage = 1;
-                } else {
-                    $currentPage = (int)$_GET['page'];
-                }
-                $thisPAgeResult = ($currentPage - 1) * $noOFResultPerPage;
-
-                $reultBooksQuery = 'SELECT * FROM NotesDetails WHERE IsActive = 1 AND Status = 9';
-                $resultBookResult = mysqli_query($connection, $reultBooksQuery);
-                $totalSearhedBooks = mysqli_num_rows($resultBookResult);
-
-
-
-
-                ?>
-
-                <div class="row heading">
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <h2>Total <?php echo $totalSearhedBooks; ?> notes</h2>
-                    </div>
-                </div>
-
-                <div class="row">
-
-                    <?php
-
-                        $showBooksQuery = 'SELECT * FROM NotesDetails WHERE IsActive = 1 AND Status = 9 LIMIT ' . $thisPAgeResult . ',' . $noOFResultPerPage;
-                        $showBookResult = mysqli_query($connection, $showBooksQuery);
-                        
-                        while ($bookDetails = mysqli_fetch_assoc($showBookResult)) {
-
-                            $bookID = $bookDetails['ID'];
-                            $bookTitle = $bookDetails['Title'];
-                            $bookUniversity = $bookDetails['UniversityName'];
-                            $bookRatings = $bookDetails['Ratings'];
-                            $bookPages = $bookDetails['NumberofPages'];
-                            $imageSRC = $bookDetails['DisplayPicture'];
-                            $countryID = $bookDetails['Country'];
-                            $totalRatings = 0;
-                            $spamReports = 0;
-
-                            $getCountry = mysqli_query($connection, "SELECT * FROM Countries WHERE ID = $countryID ");
-                            $getCountryResult = mysqli_fetch_assoc($getCountry);
-                            $country = $getCountryResult['Name'];
-
-                            $imageSRC = str_replace('../', '', $imageSRC);
-
-                            $publishedDate = $bookDetails['PublishedDate'];
-                            $dateTime = strtotime($publishedDate);
-                            $publishedDate = date("D, M d Y", $dateTime);
-
-                            $totalRatingResult = mysqli_query($connection, "SELECT * FROM NotesReviews WHERE NoteID = $bookID ");
-                            if ($totalRatingResult) {
-                                $totalRatings = mysqli_num_rows($totalRatingResult);
-                            } else {
-                                $totalRatings = "not found";
-                            }
-
-                            $totalSpamResult = mysqli_query($connection, "SELECT * FROM ReportedIssues WHERE NoteID = $bookID ");
-                            if ($totalSpamResult) {
-                                $spamReports = mysqli_num_rows($totalSpamResult);
-                            }
-
-                            echo '<div class="col-lg-4 col-md-6 col-sm-12">
-                                    <div class="book-info">
-                                        <img src="' . $imageSRC . '" class="img-responsive book-img" alt="Book">
-
-                                        <div class="book-short-info">
-
-                                            <div class="book-heading">
-                                                <a class="link-to-note-preview">
-                                                    <h5>' . $bookTitle . '</h5>
-                                                </a>
-
-                                                <input type="hidden" name="noteID" value="' . $bookID . '">
-                                                <input type="hidden" name="getNoteID">
-                                                <button type="submit" name="getNoteDetail" style="visibility:hidden">
-
-                                            </div>
-
-                                            <ul>
-                                                <li>
-                                                    <div class="book-info-img"><img src="images/search/university.png" alt="University">
-                                                    </div><span>' . $bookUniversity . ', ' . $country . '</span>
-                                                </li>
-                                                <li>
-                                                    <div class="book-info-img"><img src="images/search/pages.png" alt="Book"></div>
-                                                    <span>' . $bookPages . ' Pages</span>
-                                                </li>
-                                                <li>
-                                                    <div class="book-info-img"><img src="images/search/calendar.png" alt="calendar">
-                                                    </div><span>' . $publishedDate . '</span>
-                                                </li>
-                                                <li>
-                                                    <div class="book-info-img"><img src="images/search/flag.png" alt="Report"></div>
-                                                    <span>' . $spamReports . ' Users marked this as
-                                                        inapropriate</span>
-                                                </li>
-                                            </ul>
-
-                                            <div class="row star">
-                                                <div class="col-lg-5 col-md-5 col-sm-5">
-                                                    <img src="images/search/star.png" alt="Star">
-                                                    <img src="images/search/star.png" alt="Star">
-                                                    <img src="images/search/star.png" alt="Star">
-                                                    <img src="images/search/star.png" alt="Star">
-                                                    <img src="images/search/star.png" alt="Star">
-                                                </div>
-                                                <div class="col-lg-7 col-md-7 col-sm-7">' . $totalRatings . ' reviews</div>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>';
-                        }
-
-                    ?>
-
-                </div>
-
-            </div>
-                  <!-- Pagination -->
-        <div id="pagination">
-            <nav aria-label="Page navigation">
-                <ul class="pagination justify-content-center">
-                    <li class="page-item arrow">
-                        <a class="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&#8249;</span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                    </li>
-                    <?php
-                    $noOfPages = ceil($totalSearhedBooks / $noOFResultPerPage);
-                    for ($page = 1; $page <= $noOfPages; $page++) {
-                        echo '<li class="page-item "><a class="page-link" href="search-notes.php?page=' . $page . '">' . $page . '</a></li>';
-                    }
-                    ?>
-                    <?php ?>
-                    <li class="page-item arrow">
-                        <a class="page-link text-center" href="#" aria-label="Next">
-                            <span aria-hidden="true">&#8250;</span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <!-- paginaation Main Ends -->
-        </section>
         <!-- Search Result Ends -->
-
-        <!-- Pagination -->
-        <!-- <div id="pagination">
-            <nav aria-label="Page navigation">
-                <ul class="pagination justify-content-center">
-                    <li class="page-item arrow">
-                        <a class="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&#8249;</span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                    </li>
-                    <?php
-                    // $noOfPages = ceil($totalSearhedBooks / $noOFResultPerPage);
-                    // for ($page = 1; $page <= $noOfPages; $page++) {
-                    //     echo '<li class="page-item "><a class="page-link" href="search-notes.php?page=' . $page . '">' . $page . '</a></li>';
-                    // }
-                    ?>
-                    <li class="page-item arrow">
-                        <a class="page-link text-center" href="#" aria-label="Next">
-                            <span aria-hidden="true">&#8250;</span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div> -->
-        <!-- paginaation Main Ends -->
+<section id="search-result"></section>
+    
 
     </form>
 
@@ -447,7 +263,7 @@ session_start();
     <script src="js/bootstrap/bootstrap.min.js"></script>
 
     <script src="js/header/header.js"></script>
-    <script src="js/search-notes.js?version=1454555562"></script>
+    <script src="js/search-notes.js?version=2045245115512"></script>
 
 </body>
 
